23da1b89215ba4a26998b6bb98877e78cbb95d7e
José Pablo Southerland <jose.southerland@usm.cl>
2025-04-03 11:49:03 +0200

    header part of en-savoir-plus page

--- front/src/app/en-savoir-plus/page.tsx
+++ front/src/app/en-savoir-plus/page.tsx
@@ -15,13 +15,14 @@ export default async function Page() {
 		<main className="page">
 			<div className="container">
 				<Title name="L'empreinte numérique"/>
-				<div className="content">
-					<div className="description">
+				<div className={styles.header}>
+					<div>
 						<p>APRR, en collaboration avec des étudiants de l'INSA Lyon, nous avons développé une plateforme visant à promouvoir un numérique plus responsable. En tant que leader dans la gestion et l'exploitation d'un vaste réseau autoroutier, nous nous engageons activement dans la réduction de notre empreinte environnementale et le développement d’initiatives innovantes pour accompagner la transition écologique.</p>
 						<p>Dans cette démarche, nous avons conçu un outil simple et intuitif permettant à chacun d’évaluer l'empreinte carbone liée à ses appareils numériques (ordinateurs, smartphones) et aux services digitaux utilisés quotidiennement. Convaincus que les bonnes pratiques commencent par la sensibilisation, nous mettons également à disposition des guides et référentiels pratiques sur les gestes écoresponsables applicables au numérique. Notre objectif est d'aider les utilisateurs à mieux comprendre l'impact environnemental de leurs usages numériques et à adopter des habitudes plus respectueuses de la planète.</p>
 						<p>À travers cet outil et nos nombreuses actions pédagogiques, nous nous inscrivons comme des acteurs engagés pour un avenir numérique durable et respectueux de l'environnement.</p>
 					</div>
-					<div className="image">
+					<div>
+						<img src="/images/technologie-verte.png" alt="L'empreinte numérique" className={styles.image} />
 					</div>
 				</div>
 				<Title name="L'empreinte numérique par dispositif"/>
