c6c19d1c9ad65bc44d7693fb5d8fd5dddd3208e1
jimasr <hazimasri2002@yahoo.com>
2025-04-03 18:06:57 +0200

    chore: remove unused imports

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import Button from './components/home/button';
-import Link from 'next/link';
 
 export default function Home() {
     return (
