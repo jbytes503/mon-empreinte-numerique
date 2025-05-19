8b4143621902a4e60d75df8b7b3adbb4745a47aa
Jassir69 <jassir69310@gmail.con>
2025-05-19 22:09:40 +0200

    update of the icon

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -14,8 +14,26 @@ const spaceGrotesk = Space_Grotesk({
 export const metadata: Metadata = {
     title: 'Mon empreinte numérique',
     description: 'Un site pour évaluer votre empreinte numérique',
+    icons: {
+        icon: [
+            { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+            { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+            {
+                url: '/android-chrome-192x192.png',
+                type: 'image/png',
+                sizes: '192x192',
+            },
+            {
+                url: '/android-chrome-512x512.png',
+                type: 'image/png',
+                sizes: '512x512',
+            },
+            { url: '/favicon.ico', type: 'image/x-icon' },
+        ],
+        apple: '/apple-touch-icon.png',
+    },
+    manifest: '/site.webmanifest',
 };
-
 export default function RootLayout({
     children,
 }: Readonly<{
